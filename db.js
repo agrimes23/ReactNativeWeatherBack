e8d95f76240c223f695bb3376cb9c0f6dd4ecfe5
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-05-19 11:54:57 -0500

    setting up postgres db info

--- db.js
+++ db.js
@@ -1,1 +1,9 @@
 const Pool = require('pg')
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "", // need to set up database
+    password: "", // need to download env package
+    port: 5432,
+})
