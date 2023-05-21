1fc43dc5bccf1eaf41f885a3a2fb61835cffab55
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-05-20 23:35:13 -0500

    added a sql query in queries.js

--- src/queries.js
+++ src/queries.js
@@ -1,9 +1,11 @@
 const getWeather = "SELECT * FROM nativeweather"
 const getWeatherByCity = "SELECT * FROM nativeweather WHERE city = $1" // $1 is a param variable. 
 const checkCityExists = "SELECT s FROM nativeweather s WHERE s.city"
+const addCity = "INSERT INTO nativeweather (city) VALUES ($1)"
 
 module.exports = {
     getWeather,
     getWeatherByCity,
     checkCityExists,
+    addCity,
 }
